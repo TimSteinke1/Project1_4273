6aef94aabd068487f0470f7d6f65cfe102d24a9e
ameliaaltare <attdlplyz@gmail.com>
2023-10-27 21:01:08 -0500

    0.2 build

---
+++ Assets/Scripts/game scripts/bullet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+
+    public float speed = 20f;
+    public float damage = 1f;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.up * speed;
+    }
+
+    void OnTriggerEnter2D (Collider2D hitInfo)
+    {
+        Debug.Log(hitInfo.name);
+        Destroy(gameObject);
+    }
+}
